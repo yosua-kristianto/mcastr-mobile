7527dd8174f5cf0f594423ccc6ac422d7060e0a2
Yosua Kristianto (Legion) <yosua_kristianto144@outlook.com>
2025-11-20 11:35:57 +0700

    LinearSVC work

--- Joblib Onnx Converter/Joblib_Onnx_Converter.py
+++ Joblib Onnx Converter/Joblib_Onnx_Converter.py
@@ -1,22 +1,24 @@
-import joblib
-from skl2onnx import convert_sklearn
-from skl2onnx.common.data_types import FloatTensorType
 
-model = joblib.load("model.joblib")
+if __name__ == "__main__":
+    import joblib
+    from skl2onnx import convert_sklearn
+    from skl2onnx.common.data_types import FloatTensorType
 
-print("n_features_in =", model.n_features_in_)
-print("Number of support vectors =", len(model.support_))
+    model = joblib.load("model.joblib")
 
-# If you only saved SVC(model), wrap manually:
-# model = Pipeline([("svc", model)])
+    print("n_features_in =", model.n_features_in_)
+    # print("Number of support vectors =", len(model.support_))
 
-n_features = model.n_features_in_
+    # If you only saved SVC(model), wrap manually:
+    # model = Pipeline([("svc", model)])
 
-initial_type = [("input", FloatTensorType([None, n_features]))]
+    n_features = model.n_features_in_
 
-onnx_model = convert_sklearn(model, initial_types=initial_type)
+    initial_type = [("input", FloatTensorType([None, n_features]))]
 
-with open("model.onnx", "wb") as f:
-    f.write(onnx_model.SerializeToString())
+    onnx_model = convert_sklearn(model, initial_types=initial_type)
 
-print("SVC (probability=True) exported to ONNX successfully.")
+    with open("model.onnx", "wb") as f:
+        f.write(onnx_model.SerializeToString())
+
+    print("SVC (probability=True) exported to ONNX successfully.")
